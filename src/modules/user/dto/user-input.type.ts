d116acd88b35b8dee538cd396d2cdea45ce6107c
wuwenyu <johnnywwy@gmail.com>
2024-05-21 18:26:16 +0800

    feat：实现修改接口

--- src/modules/user/dto/user-input.type.ts
+++ src/modules/user/dto/user-input.type.ts
@@ -3,9 +3,9 @@ import { Field, InputType } from '@nestjs/graphql';
 @InputType()
 export class UserInput {
   @Field({ description: '昵称' })
-  name?: string;
+  name: string;
   @Field({ description: '简介' })
   desc: string;
-  @Field({ description: '头像' })
-  avatar: string;
+  @Field({ description: '头像', nullable: true })
+  avatar?: string;
 }
